72288e146aa30da06af11120d3bf56d9995eef4e
user <user@w347447.local>
2017-09-27 19:30:19 -0400

    Initial Commit

---
+++ TP01_TempConvert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TP01_TempConvert
+//
+//  Created by user on 9/27/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
